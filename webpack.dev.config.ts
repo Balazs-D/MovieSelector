b119c73aec2bc935cb17692125dae2f7b440ea66
Balazs-D <balazs.danyadi@gmail.com>
2022-06-12 21:54:41 +0200

    wepack img loader update 2

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -1,70 +1,81 @@
 import path from "path";
-import { Configuration as WebpackConfiguration, HotModuleReplacementPlugin } from "webpack";
-import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import {Configuration as WebpackConfiguration, HotModuleReplacementPlugin} from "webpack";
+import {Configuration as WebpackDevServerConfiguration} from 'webpack-dev-server';
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import ESLintPlugin from "eslint-webpack-plugin";
 
 interface Configuration extends WebpackConfiguration {
-  devServer?: WebpackDevServerConfiguration;
+    devServer?: WebpackDevServerConfiguration;
 }
 
 const config: Configuration = {
-  mode: "development",
-  output: {
-    publicPath: "/",
-  },
-  entry: "./src/index.tsx",
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react",
-              "@babel/preset-typescript",
-            ],
-          },
-        },
-      },
-      { test: /\.css$/, use: 'css-loader' },
-      {
-        test: /\.(jpe?g|png|gif|woff|woff2|otf|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              limit: 1000,
-              name : 'assets/[name].[ext]'
+    mode: "development",
+    output: {
+        publicPath: "/",
+    },
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript",
+                        ],
+                    },
+                },
+            },
+            {test: /\.css$/, use: 'css-loader'},
+            {
+                test: /\.(jpg|jpeg|gif|png)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        publicPath: 'assets',
+                        outputPath: 'assets',
+                    }
+                }
+            },
+            {
+                test: /\.(jpe?g|png|gif|woff|woff2|otf|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1000,
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
             }
-          }
-        ]
-      }
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+        }),
+        new HotModuleReplacementPlugin(),
+        new ESLintPlugin({
+            extensions: ["js", "jsx", "ts", "tsx"],
+        }),
     ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-    new HotModuleReplacementPlugin(),
-    new ESLintPlugin({
-      extensions: ["js", "jsx", "ts", "tsx"],
-    }),
-  ],
-  devtool: "inline-source-map",
-  devServer: {
-    static: path.join(__dirname, "build"),
-    historyApiFallback: true,
-    port: 4000,
-    open: true,
-    hot: true
-  },
+    devtool: "inline-source-map",
+    devServer: {
+        static: path.join(__dirname, "build"),
+        historyApiFallback: true,
+        port: 4000,
+        open: true,
+        hot: true
+    },
 };
 
 export default config;
